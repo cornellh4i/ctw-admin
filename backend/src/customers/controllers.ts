d5a392d2fbfc2dd89e6ed1916e6928c2ccd91a25
Kaden Lei <kl646@cornell.edu>
2023-10-13 15:23:11 -0400

    Made date params optional

--- backend/src/customers/controllers.ts
+++ backend/src/customers/controllers.ts
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
-import { Cluster, ClusterModel, Net, NetModel, Data, DataModel } from "./models";
+import {
+  Cluster,
+  ClusterModel,
+  Net,
+  NetModel,
+  Data,
+  DataModel,
+} from "./models";
+import { type } from "os";
 
 /**
  * Finds all cluster docs in DB
@@ -13,20 +21,19 @@ const getClusters = async () => ClusterModel.find({});
  * @param location list of locations
  * @returns promise with new cluster doc or error
  */
-const insertCluster = async (
-  location: [number]
-) => ClusterModel.create(new Cluster(location));
+const insertCluster = async (location: [number]) =>
+  ClusterModel.create(new Cluster(location));
 
 /**
  * Removes cluster by id from DB
  * @param id cluster id
  * @returns doc containg bool acknowledged and number deletedCount
  */
-const deleteCluster = async (id : string) => {
-  ClusterModel.deleteOne({_id : new mongoose.Types.ObjectId(id)});
-  NetModel.deleteMany({clusterID : id}); // FIX: make sure all associated nets are deleted when cluster is deleted
+const deleteCluster = async (id: string) => {
+  ClusterModel.deleteOne({ _id: new mongoose.Types.ObjectId(id) });
+  NetModel.deleteMany({ clusterID: id }); // FIX: make sure all associated nets are deleted when cluster is deleted
   // DataModel.deleteMany({netID : id}); // FIX: delete data docs associated with above nets (idk how to do this rn)
-}
+};
 
 /**
  * Finds all net docs in DB
@@ -48,20 +55,19 @@ const getNetByClusterId = async (id: string) =>
  * @param type type of net
  * @returns promise with new net doc or error
  */
-const insertNet = async (
-  clusterID: string,
-  type: string
-) => NetModel.create(new Net(clusterID, type));
+const insertNet = async (clusterID: string, type: string) =>
+  NetModel.create(new Net(clusterID, type));
 
 /**
  * Removes net by id from DB
  * @param id net id
  * @returns doc containg bool acknowledged and number deletedCount
  */
-const deleteNet = async ( id : string) => {
-  NetModel.deleteOne({_id : new mongoose.Types.ObjectId(id)});
-  DataModel.deleteMany({netID : id}); // FIX: for some reason the data is not deleted
-}
+const deleteNet = async (id: string) => {
+  console.log(id);
+  NetModel.deleteOne({ _id: new mongoose.Types.ObjectId(id) });
+  DataModel.deleteMany({ netID: id }); // FIX: for some reason the data is not deleted
+};
 
 /**
  * Finds all data docs in DB
@@ -74,8 +80,7 @@ const getData = async () => DataModel.find({});
  * @param id net id
  * @returns promise with data doc or error
  */
-const getDataByNetId = async (id: string) =>
-  DataModel.find({ netID: id });
+const getDataByNetId = async (id: string) => DataModel.find({ netID: id });
 
 /**
  * Inserts new data into DB
@@ -84,19 +89,16 @@ const getDataByNetId = async (id: string) =>
  * @param water_collected amount of water collected
  * @returns promise with new data doc or error
  */
-const insertData = async (
-  netID: string,
-  date: Date,
-  water_collected: number
-) => DataModel.create(new Data(netID, date, water_collected));
+const insertData = async (netID: string, date: Date, water_collected: number) =>
+  DataModel.create(new Data(netID, date, water_collected));
 
 /**
  * Removes data by id from DB
  * @param id data id
  * @returns doc containg bool acknowledged and number deletedCount
  */
-const deleteData = async ( id : string) =>
-  DataModel.deleteOne({_id : new mongoose.Types.ObjectId(id)});
+const deleteData = async (id: string) =>
+  DataModel.deleteOne({ _id: new mongoose.Types.ObjectId(id) });
 
 /**
  * Finds all data docs from clusters in list clusterIds
@@ -107,24 +109,33 @@ const deleteData = async ( id : string) =>
  */
 const getAllDocsByClusterIDs = async (
   clusterIds: string[],
-  minDate: Date,
-  maxDate: Date
+  minDate?: Date,
+  maxDate?: Date
 ) => {
-  console.log(clusterIds)
-  const cursor1 = NetModel.find({clusterID: {"$in": clusterIds}});
+  console.log(clusterIds);
+  if (typeof minDate === undefined) {
+    minDate = new Date("2023-01-01");
+  }
+  if (typeof maxDate === undefined) {
+    maxDate = new Date(new Date().toJSON().slice(0, 10));
+  }
+  const cursor1 = NetModel.find({ clusterID: { $in: clusterIds } });
   const netIds = [];
-  for await(const doc of cursor1) {
-    netIds.push(doc.id)
+  for await (const doc of cursor1) {
+    netIds.push(doc.id);
   }
   console.log(netIds);
-  const cursor2 = DataModel.find({netID: {"$in": netIds}, date: { $gte: minDate, $lte: maxDate }});
+  const cursor2 = DataModel.find({
+    netID: { $in: netIds },
+    date: { $gte: minDate, $lte: maxDate },
+  });
   const datas = [];
-  for await(const doc of cursor2) {
-    datas.push(doc)
+  for await (const doc of cursor2) {
+    datas.push(doc);
   }
-  console.log(datas)
+  console.log(datas);
   return datas;
-}
+};
 
 export default {
   getClusters,
@@ -138,5 +149,5 @@ export default {
   getDataByNetId,
   insertData,
   deleteData,
-  getAllDocsByClusterIDs
+  getAllDocsByClusterIDs,
 };
--- backend/src/customers/views.ts
+++ backend/src/customers/views.ts
@@ -16,22 +16,12 @@ docRouter.post("/", async (req, res) => {
   const { location } = req.body;
   res
     .status(201)
-    .send(
-      successJson(
-        await DocController.insertCluster(location)
-      )
-    );
+    .send(successJson(await DocController.insertCluster(location)));
 });
 
 docRouter.delete("/delete/:id", async (req, res) => {
   const id = req.params.id;
-  res
-    .status(201)
-    .send(
-      successJson(
-        await DocController.deleteCluster(id)
-      )
-    );
+  res.status(201).send(successJson(await DocController.deleteCluster(id)));
 });
 
 docRouter.get("/nets/", async (req, res) => {
@@ -40,31 +30,19 @@ docRouter.get("/nets/", async (req, res) => {
 
 docRouter.get("/nets/:id", async (req, res) => {
   const id = req.params.id;
-  res
-    .status(200)
-    .send(successJson(await DocController.getNetByClusterId(id)));
+  res.status(200).send(successJson(await DocController.getNetByClusterId(id)));
 });
 
 docRouter.post("/nets/", async (req, res) => {
   const { clusterID, type } = req.body;
   res
     .status(201)
-    .send(
-      successJson(
-        await DocController.insertNet(clusterID, type)
-      )
-    );
+    .send(successJson(await DocController.insertNet(clusterID, type)));
 });
 
 docRouter.delete("/net/delete/:id", async (req, res) => {
   const id = req.params.id;
-  res
-    .status(201)
-    .send(
-      successJson(
-        await DocController.deleteNet(id)
-      )
-    );
+  res.status(201).send(successJson(await DocController.deleteNet(id)));
 });
 
 docRouter.get("/data/", async (req, res) => {
@@ -73,9 +51,7 @@ docRouter.get("/data/", async (req, res) => {
 
 docRouter.get("/data/:id", async (req, res) => {
   const id = req.params.id;
-  res
-    .status(200)
-    .send(successJson(await DocController.getDataByNetId(id)));
+  res.status(200).send(successJson(await DocController.getDataByNetId(id)));
 });
 
 docRouter.post("/data/", async (req, res) => {
@@ -83,34 +59,37 @@ docRouter.post("/data/", async (req, res) => {
   res
     .status(201)
     .send(
-      successJson(
-        await DocController.insertData(netID, date, water_collected)
-      )
+      successJson(await DocController.insertData(netID, date, water_collected))
     );
 });
 
 docRouter.delete("/data/delete/:id", async (req, res) => {
   const id = req.params.id;
-  res
-    .status(201)
-    .send(
-      successJson(
-        await DocController.deleteData(id)
-      )
-    );
+  res.status(201).send(successJson(await DocController.deleteData(id)));
 });
 
-const clusterIds = ["65258f01ba4c14948edd4526", "65258d88899b7e221c1d33eb"] // NOTE: temp hardcoded list of clusterIDs for testing
-docRouter.get("/:minDate/:maxDate/", async (req, res) => {
+const clusterIds = ["65258f01ba4c14948edd4526", "65258d88899b7e221c1d33eb"]; // NOTE: temp hardcoded list of clusterIDs for testing
+docRouter.get("/:minDate?/:maxDate?/", async (req, res) => {
   let minDate = new Date(); // FIX: right now minDate and maxDate are required fields but they should be able to be left empty
   let maxDate = new Date();
-  if (req.params.minDate) {minDate = new Date(req.params.minDate)}
-  else {minDate = new Date("2023-01-01")};
-  if (req.params.maxDate) {maxDate = new Date(req.params.maxDate)} // Assumes the date is valid with correct syntax
-  else {maxDate = new Date(new Date().toJSON().slice(0, 10))};
+  if (req.params.minDate) {
+    minDate = new Date(req.params.minDate);
+  } else {
+    minDate = new Date("2023-01-01");
+  }
+  if (req.params.maxDate) {
+    maxDate = new Date(req.params.maxDate);
+  } // Assumes the date is valid with correct syntax
+  else {
+    maxDate = new Date(new Date().toJSON().slice(0, 10));
+  }
   res
     .status(200)
-    .send(successJson(await DocController.getAllDocsByClusterIDs(clusterIds, minDate, maxDate)));
+    .send(
+      successJson(
+        await DocController.getAllDocsByClusterIDs(clusterIds, minDate, maxDate)
+      )
+    );
 });
 
 export default docRouter;
