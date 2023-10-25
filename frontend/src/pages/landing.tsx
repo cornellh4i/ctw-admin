90317289783a550252cd2146c5f2dc5844d66693
mohammad0612 <mai212228@gmail.com>
2023-10-24 21:24:59 -0400

    debugging button issue

---
+++ frontend/src/pages/landing.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+const MyButton: React.FC = () => {
+  return (
+  <Button variant="contained">Hello world</Button>
+  );
+}
+export default MyButton;
