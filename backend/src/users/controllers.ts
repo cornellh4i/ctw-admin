79687a1ef39f88ed6f92ad12f0262c64e32c2c2a
Jiho Lee <jiho.lee17@gmail.com>
2023-10-30 16:03:57 -0400

    create function and route to modify user claims

--- backend/src/users/controllers.ts
+++ backend/src/users/controllers.ts
@@ -1,1 +1,27 @@
-// Controllers here, following the pattern in ../customers/controllers.ts
+import * as admin from 'firebase-admin'
+
+const serviceAccount = require('../../serviceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+
+/**
+ * Modify the claim of a user given user ID and new claim
+ */
+export function changeClaim(uid: string, new_claim: number) {
+  const claims = {
+    mobile_entry: new_claim == 0,
+    web_entry: new_claim == 1,
+    evaluator: new_claim == 2,
+    admin: new_claim == 3
+  };
+  admin.auth().setCustomUserClaims(uid, claims)
+  .catch(
+    () => {
+      console.log("ERROR: User with uid " + uid + " not found.");
+    }
+  )
+}
+
+export default { changeClaim };
