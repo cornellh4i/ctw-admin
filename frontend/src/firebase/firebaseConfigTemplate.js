00ee5ae53d5ac9cb7fcd630f664199f32189cbca
leaneying <leaneying23@gmail.com>
2023-10-30 15:59:08 -0400

    made firebase folder

---
+++ frontend/src/firebase/firebaseConfigTemplate.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+
+};
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
