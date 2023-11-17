255fb1397d007d9ab247c90ba7933c2b2fbeac35
Jiho Lee <jiho.lee17@gmail.com>
2023-11-17 15:03:42 -0500

    initial d3 component

--- frontend/src/pages/data-viz.tsx
+++ frontend/src/pages/data-viz.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import MapChart from "../components/MapChart";
+import LineGraph from "../components/LineGraph";
 
-const Data_viz = ({
-}) => {
-
+const Data_viz = ({}) => {
   const bottomLeft = [0, 0];
   const topRight = [30, 40];
-  const markers = [[0, 0], [15, 20], [30, 40]];  
+  const markers = [
+    [0, 0],
+    [15, 20],
+    [30, 40],
+  ];
+
+  const data = [
+    { date: "2023-01-01", waterQuantity: 50, variable1: 25, variable2: 30 },
+    { date: "2023-01-02", waterQuantity: 60, variable1: 35, variable2: 40 },
+    // Add more data objects as needed
+  ];
+
   return (
-    <div>
-      <h1>Data_viz</h1>
-      <MapChart bottomLeft={bottomLeft} topRight={topRight} markers={markers} />
-    </div>
+    <>
+      <div>
+        <h1>Data_viz</h1>
+        <MapChart
+          bottomLeft={bottomLeft}
+          topRight={topRight}
+          markers={markers}
+        />
+      </div>
+      <div>
+        <LineGraph data={data}></LineGraph>
+      </div>
+    </>
   );
 };
 
