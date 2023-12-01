2b10340d1ec5b37ffc92e627f3abb8bd1fff4eae
Kaden Lei <kl646@cornell.edu>
2023-12-01 15:15:17 -0500

    Started working on it

--- frontend/src/components/MapChart.jsx
+++ frontend/src/components/MapChart.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
-
+import { useEffect } from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 
 const MapChart = ({ bottomLeft, topRight, markers }) => {
   return (
     <div style={{ flex: 1, margin: 20 }}>
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
-      <MapContainer center={[topRight[0] - bottomLeft[0] / 2, topRight[1] - bottomLeft[1] / 2]} zoom={6} scrollWheelZoom={true} style={{ height: "100%" }}>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css"
+      />
+      <MapContainer
+        center={[
+          topRight[0] - bottomLeft[0] / 2,
+          topRight[1] - bottomLeft[1] / 2,
+        ]}
+        zoom={6}
+        scrollWheelZoom={true}
+        style={{ height: "100%" }}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- frontend/src/pages/data-viz.tsx
+++ frontend/src/pages/data-viz.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import MapChart from '../components/MapChart';
-import AltitudeComponent from '../components/altitude-component';
-import MeshSelector from '../components/MeshSelector';
-import GraphCard from '../components/GraphCard';
-import LocationSelector from '../components/LocationSelector';
+import { useEffect } from "react";
+import MapChart from "../components/MapChart";
+import AltitudeComponent from "../components/altitude-component";
+import MeshSelector from "../components/MeshSelector";
+import GraphCard from "../components/GraphCard";
+import LocationSelector from "../components/LocationSelector";
+
+const fetchClustersHandler = async () => {
+  try {
+    const response = await fetch("http://localhost:8000/clusters");
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const Data_viz = () => {
+  useEffect(() => {
+    fetchClustersHandler();
+  }, []);
   const bottomLeft = [-10, -100];
   const topRight = [-14, -104];
   const markers = [
@@ -17,23 +34,23 @@ const Data_viz = () => {
     <div>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
         }}
       >
-        <MeshSelector typeList={['test1', 'test2']} />
-        <LocationSelector typeList={['location1', 'location2', 'location 3']} />
-        <MeshSelector typeList={['test1', 'test2']} />
+        <MeshSelector typeList={["test1", "test2"]} />
+        <LocationSelector typeList={["location1", "location2", "location 3"]} />
+        <MeshSelector typeList={["test1", "test2"]} />
         <AltitudeComponent minAlt={1000} maxAlt={2000} />
       </div>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
         <MapChart
           bottomLeft={bottomLeft}
           topRight={topRight}
           markers={markers}
         />
-        <GraphCard title='graph goes here' />
+        <GraphCard title="graph goes here" />
       </div>
     </div>
   );
